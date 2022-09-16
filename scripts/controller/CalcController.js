199256baff5a773a2164a517d9bb73ef135310e0
zYumoto <victorjuanrazer@gmail.com>
2022-09-16 14:55:03 -0300

    set date and time success

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,66 +1,67 @@
-class CalcController{
-    
-        constructor(){
-            this._locale = "pT-BR";
-            this._displayCalcEl = document.querySelector("#display");
-            this._dateEl = document.querySelector("#date");
-            this._timeEl = document.querySelector("#time");
-            this._currentDate;
-            this.initialize();
-        }
+class CalcController {
 
-        initialize(){
+    constructor(){
 
-            this.setDisplayDateTime()
+        this._locale = "pt-BR";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
 
-            setInterval(() => {
-                
-                this.setDisplayDateTime();
+    }
 
-            }, 1000);
-          
+    initialize(){
 
-        }
+        this.setDisplayDateTime()
 
-        setDisplayDateTime(){
+        setInterval(()=>{
 
-            this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
-                day: "2-digit",
-                month: "long",
-                year: "numeric"
-            });
-            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
-    
-        }
+            this.setDisplayDateTime();
 
-        get displayTime(){
-            return this._timeEl.innerHTML;
-        }
-        set displayTime(value){
-            return this._timeEl.innerHTML = value;
-        }
- 
-        get displayDate(){
-            return this._dateEl.innerHTML;
-        }
-        set displayDate(value){
-            return this._dateEl.innerHTML = value;
-        }
+        }, 1000);
 
+    }
 
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
 
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
 
-        get displayCalc(){
-            return this._displayCalcEl.innerHTML;
-        }
-        set displayCalc(value){
-            this._displayCalcEl.innerHTML = value;
-        }
-        get currentDate(){
-            return new Date ();
-        }
-        set currentDate(value){
-            this._currentDate = value;
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+    }
 
-        }
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
+    }
+
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate(){
+        return new Date();
+    }
+
+    set currentDate(value){
+        this._currentDate = value;
+    }
+
 }

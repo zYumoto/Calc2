62d8e9c3433fa5edd61713c05915ff6f6482de66
zYumoto <victorjuanrazer@gmail.com>
2022-09-16 15:31:57 -0300

    swicth and an event for each button

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,17 +2,19 @@ class CalcController {
 
     constructor(){
 
+        this._operation = [];
         this._locale = "pt-BR";
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
 
     }
 
     initialize(){
-
+//ter um intervalo de 1segundo para ir atualizando
         this.setDisplayDateTime()
 
         setInterval(()=>{
@@ -23,6 +25,125 @@ class CalcController {
 
     }
 
+
+    
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event => {
+
+            element.addEventListener(event, fn, false);
+
+        })
+    
+    }
+//SWITCH DOS BOTOES 
+    clearAll(){
+
+        this._operation = [];
+
+    }
+
+    clearEntry(){
+
+        this._operation.pop();
+
+    }
+
+    addOperation(value){
+
+        this._operation.push(value);
+
+        console.log(this._operation);
+
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+        
+    }
+
+    execBtn(value){
+
+        switch (value) {
+
+            case 'ac':
+                this.clearAll();
+                break;
+
+            case 'ce':
+                this.clearEntry();
+                break;
+
+            case 'soma':
+                
+                break;
+
+            case 'subtracao':
+                
+                break;
+
+            case 'divisao':
+                
+                break;
+
+            case 'porcento':
+                
+                break;
+
+            case 'igual':
+                
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+
+        }
+
+    }
+
+//EVENTOS DOS BOTOES
+initButtonsEvents(){
+
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+    buttons.forEach((btn, index)=>{
+
+        this.addEventListenerAll(btn, "click drag", e => {
+
+            let textBtn = btn.className.baseVal.replace("btn-","");
+
+            this.execBtn(textBtn);
+
+        })
+
+        this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
+            btn.style.cursor = "pointer";
+
+        })
+
+    })
+
+}
+
+
+//DATA E HORA
+//aparecer a data e hora por extenço
     setDisplayDateTime(){
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: "2-digit",
@@ -32,6 +153,8 @@ class CalcController {
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
+
+
     get displayTime(){
         return this._timeEl.innerHTML;
     }
@@ -39,6 +162,8 @@ class CalcController {
     set displayTime(value){
         return this._timeEl.innerHTML = value;
     }
+
+
 
     get displayDate(){
         return this._dateEl.innerHTML;
@@ -48,6 +173,8 @@ class CalcController {
         return this._dateEl.innerHTML = value;
     }
 
+
+
     get displayCalc(){
         return this._displayCalcEl.innerHTML;
     }
@@ -55,6 +182,8 @@ class CalcController {
     set displayCalc(value){
         this._displayCalcEl.innerHTML = value;
     }
+
+
 
     get currentDate(){
         return new Date();
